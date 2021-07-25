f4edbaae16323fdc13f9342ed6e39f2b8600304a
Marios Pitsalidis <marios.pitsali@gmail.com>
2021-07-25 13:47:19 +0300

    imported tqdm, ready to conditionally remove files

--- automations/practice.py
+++ automations/practice.py
@@ -1,22 +1,33 @@
-#%%
-import sys, os
-from collections import namedtuple
+
+import os
 from datetime import datetime
+from tqdm import tqdm, trange
 
-root_folder = r"C:\Users\User\Downloads"
-# %%
-for item in os.scandir(root_folder):
-    if item.name.endswith(".rar") or item.name.endswith(".zip"):
-        item_path = os.path.join(root_folder, item.name)
+if __name__ == "__main__":
 
-        print (f'{item.name} is at location {item_path}.')
+    downloads_folder = r"C:\Users\User\Downloads"
 
-        if os.path.exists(item_path):
-            size_of_file = os.path.getsize(item_path)
-            date_created = datetime.fromtimestamp(os.path.getctime(item_path))
-            date_created_formatted = date_created.strftime('%Y-%m-%d %H:%M:%S')
-            print (f'File {item.name} has size of {size_of_file/1000} KB and was created at {date_created_formatted}')
-            print (datetime.today())
+    downloads_files_list = [item.name for item in os.scandir(downloads_folder)]
+
+    for item in tqdm(downloads_files_list, desc="Scanning Downloads Files...", colour='red', unit=" files scanned"):
+
+        if item.endswith(".rar") or item.endswith(".zip"):
+            item_path = os.path.join(downloads_folder, item)
+
+            print (f'{item} is at location {item_path}.')
+
+            if os.path.exists(item_path):
+                size_of_file = os.path.getsize(item_path)
+                date_created = datetime.fromtimestamp(os.path.getctime(item_path))
+                date_created_formatted = date_created.strftime('%Y-%m-%d %H:%M:%S')
+                date_modified = datetime.fromtimestamp(os.path.getmtime(item_path))
+                date_modified_formatted = date_modified.strftime('%Y-%m-%d %H:%M:%S')
+                print (date_created_formatted, date_modified_formatted)
+                #print (f'File {item.name} has size of {size_of_file/1000} KB and was created at {date_created_formatted}')
+                
+                #delete all files that have been extracted and are older than six months
+                datetime_now = datetime.today()
+
+                print (f'File {item} was created {(datetime_now - date_created)} ago.')
+                print (type((datetime_now - date_created)))
             
-
-# %%
