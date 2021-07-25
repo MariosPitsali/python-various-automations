d6335fdef273105fa9a011fd96e6bd1e85f3d134
Marios Pitsalidis <marios.pitsali@gmail.com>
2021-07-25 14:08:18 +0300

    added hd space saving message

--- automations/practice.py
+++ automations/practice.py
@@ -1,4 +1,3 @@
-
 import os
 from datetime import datetime
 from tqdm import tqdm, trange
@@ -6,12 +5,13 @@ from tqdm import tqdm, trange
 if __name__ == "__main__":
 
     downloads_folder = r"C:\Users\User\Downloads"
-
     downloads_files_list = [item.name for item in os.scandir(downloads_folder)]
+
+    total_mb_saved = 0
 
     for item in tqdm(set(downloads_files_list), desc="Scanning Downloads Files...", colour='red'):
 
-        if item.endswith(".rar") or item.endswith(".zip"):
+        if item.endswith(".xlsx") or item.endswith(".xls"):
             item_path = os.path.join(downloads_folder, item)
 
             if os.path.exists(item_path):
@@ -19,14 +19,18 @@ if __name__ == "__main__":
                 size_of_file = os.path.getsize(item_path)
 
                 date_created = datetime.fromtimestamp(os.path.getctime(item_path))
-                date_created_formatted = date_created.strftime('%Y-%m-%d %H:%M:%S')
+                date_created_formatted = date_created.strftime('%Y-%m-%d %H:%M:%S')#formatted for output purposes
                 date_modified = datetime.fromtimestamp(os.path.getmtime(item_path))
-                date_modified_formatted = date_modified.strftime('%Y-%m-%d %H:%M:%S')
-                
-                
+                date_modified_formatted = date_modified.strftime('%Y-%m-%d %H:%M:%S')#formatted for output purposes
+                               
                 #delete all files that are older than 3 months
+
                 datetime_now = datetime.today()
-                if (datetime_now - date_created).days > 100:
+                time_difference = datetime_now - date_created
+
+                if time_difference.days > 30:
+                    total_mb_saved += round(size_of_file/1024.0 * 1024.0)
                     os.remove(item_path)
                     #print (f'File {item.name}, with size of {size_of_file/1000} KB and was created at {date_created_formatted}, was deleted')
-            
+    
+    print (f'Approx. {total_mb_saved} MB saved from hard drive.')       
