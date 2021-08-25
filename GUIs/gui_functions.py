eae3b0202fba1a95c846d9d089ebac783d66feb5
Marios Pitsalidis <marios.pitsali@gmail.com>
2021-08-25 18:13:13 +0300

    add new functions

--- GUIs/gui_functions.py
+++ GUIs/gui_functions.py
@@ -30,7 +30,11 @@ def display_image_content_in_file(folder_path:str):
     Displays state of all image files in given folder, with relevant file details."""
 
     def display_next():
-        pass
+        image_label.grid_forget()
+        image_details_label.grid_forget()
+
+        new_image_label = Label(image = ImageTk.PhotoImage(image_list[1]))
+        new_image_label.grid(row=1, column = 2)
 
     def display_previous():
         pass
@@ -42,11 +46,11 @@ def display_image_content_in_file(folder_path:str):
     try:
         image_list = [Image.open(os.path.join(folder_path, item.name)) for item in os.scandir(folder_path) if item.name.endswith('jpg')]
 
-        img = ImageTk.PhotoImage(image_list[2])
+        img = ImageTk.PhotoImage(image_list[0])
         image_label = Label(image = img)
-        image_details_label = Label(text = 'Image Size: {}, Image Format: {}.'.format(image_list[2].size, image_list[2].format))
-        image_label.grid(row = 1, column = 1)
-        image_details_label.grid(row = 2, column = 1)
+        image_details_label = Label(text = 'Image Size: {}, Image Format: {}.'.format(image_list[0].size, image_list[0].format))
+        image_label.grid(row = 1, column = 2)
+        image_details_label.grid(row = 2, column = 2)
 
         # for i, image in enumerate(image_list):
         #     tk_image = ImageTk.PhotoImage(image)
@@ -58,6 +62,15 @@ def display_image_content_in_file(folder_path:str):
         label = Label(text = "Ooops")
         label.pack()
     
+
+    button_next = Button(root, text=">>", command = display_next)
+    button_back = Button(root, text="<<", command = display_previous)
+    button_close = Button(root, text="Close", command = root.destroy)
+
+    button_back.grid(column = 1, row = 3)
+    button_next.grid(column = 3, row = 3)
+    button_close.grid(column = 2, row = 3)
+
     root.mainloop()
 
 display_image_content_in_file(r"C:\Users\User\Desktop\LIEBE ANTA")
